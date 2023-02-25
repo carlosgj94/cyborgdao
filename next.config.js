6ade7317d62ce83a8962846bb6a8a55f5367fcf7
Carlos Juarez <carlosgj94@gmail.com>
2023-02-25 13:00:42 +0100

    🔥: starting the new mint in goerli!

--- next.config.js
+++ next.config.js
@@ -4,9 +4,11 @@ module.exports = {
       domains: ['ipfs.io'],
   },
   env: {
-    chainId: 1,
-    contractAddress: '0xc0c9c701530246712F42b42c0E8E3CD9ec324adF',
+    //chainId: 1,
+    // contractAddress: '0xc0c9c701530246712F42b42c0E8E3CD9ec324adF',
     mintPrice: '0.5',
+    chainId: 5,
+    contractAddress: '0x46215bC3663eB9a17DeBC6A70861CA5017c11e8C',
     nftImage: 'https://ipfs.io/ipfs/QmZme7YDRKZ9EoeNrmkHhFoXTWfyxFoASAJC6jcwNQw5RL',
   },
 }
