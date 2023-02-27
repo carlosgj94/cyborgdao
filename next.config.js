f3c2a0887b40e7c60f3af748cb11a1fe55526282
Carlos Juarez <carlosgj94@gmail.com>
2023-02-27 23:29:42 +0000

    🔥: mainnet v2

--- next.config.js
+++ next.config.js
@@ -4,11 +4,10 @@ module.exports = {
       domains: ['ipfs.io'],
   },
   env: {
-    //chainId: 1,
     // contractAddress: '0xc0c9c701530246712F42b42c0E8E3CD9ec324adF',
-    mintPrice: '0.5',
-    chainId: 5,
-    contractAddress: '0x46215bC3663eB9a17DeBC6A70861CA5017c11e8C',
+    mintPrice: '1',
+    chainId: 1,
+    contractAddress: '0x92540A41CF6406a88Eee9DA5D233FbD0e82Bd529',
     nftImage: 'https://ipfs.io/ipfs/QmZme7YDRKZ9EoeNrmkHhFoXTWfyxFoASAJC6jcwNQw5RL',
   },
 }
