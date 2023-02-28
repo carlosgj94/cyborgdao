b0c0970a08b9ab42294c6d7a76b49a34c1135734
PiGallego <32301192+xPi2@users.noreply.github.com>
2023-02-28 20:02:13 +0000

    🔧: revert to previous contract

--- next.config.js
+++ next.config.js
@@ -4,10 +4,9 @@ module.exports = {
       domains: ['ipfs.io'],
   },
   env: {
-    // contractAddress: '0xc0c9c701530246712F42b42c0E8E3CD9ec324adF',
     mintPrice: '1',
     chainId: 1,
-    contractAddress: '0x92540A41CF6406a88Eee9DA5D233FbD0e82Bd529',
+    contractAddress: '0xc0c9c701530246712F42b42c0E8E3CD9ec324adF',
     nftImage: 'https://ipfs.io/ipfs/QmZme7YDRKZ9EoeNrmkHhFoXTWfyxFoASAJC6jcwNQw5RL',
   },
 }
